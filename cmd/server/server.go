b032858de91837e262a46982ec31c27449d79085
Chansung Park <deep.diver.csp@gmail.com>
2019-07-22 15:36:38 +0900

    adding some comments

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"log"
-	"net/http"
+	"net/http" // http package
 	"os"
 
 	"github.com/dl4ab/timebot/api"
 	"github.com/dl4ab/timebot/api/slack"
 )
 
+/*
+	look up environment variable existence
+*/
 func mustLookupEnv(env string) string {
 	ret, ok := os.LookupEnv(env)
 
@@ -29,8 +32,14 @@ func main() {
 	slackToken := mustLookupEnv("SLACK_SIGNING_SECRET")
 	slackBotOAuthToken := mustLookupEnv("SLACK_BOT_OAUTH_TOKEN")
 
+	// slack is just a struct to hold slack related tokens
 	app := slack.New(slackToken, slackBotOAuthToken)
 
+	// http.ListenAndServe creates HTTP server
+	// 	- need to specify a port to listen to
+	//	- need to specify http.NewServeMux() instance to map current request to the designated route 
+	// check api package in github.com/dl4ab/timebot/ for more details
+	// check more about it [https://golang.org/pkg/net/http/]
 	log.Printf("[MAIN] The server is running at 0.0.0.0:%v\n", port)
 	log.Println("[MAIN]", http.ListenAndServe(":"+port, api.GetRouter(app)))
 }
